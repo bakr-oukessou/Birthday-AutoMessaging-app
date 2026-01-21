6d23a2fd8aab8cd778c363c5359d2be68590eded
bakr-oukessou <bakr.oukessou@gmail.com>
2026-01-21 21:18:53 +0100

    feat: update app structure and dependencies for improved performance

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -5,8 +5,7 @@ import { AuthProvider } from './src/context';
 import { AppNavigator } from './src/navigation';
 
 export default function App() {
-  return (
-    <SafeAreaProvider>
+  return (                                                                                                                         <SafeAreaProvider>
       <AuthProvider>
         <StatusBar style="auto" />
         <AppNavigator />
